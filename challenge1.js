e1c66311f06a4a9a25cbfac1a5962e36d9a533ad
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-09 21:02:57 -0800

    touch challenge1.js

---
+++ challenge1.js
@@ -0,0 +1,29 @@
+function FirstReverse(str) { 
+
+  // code goes here  
+  return str; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
