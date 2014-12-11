da20ce7caf6fb205905eb4bddedb619601f69bc6
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-11 01:51:28 -0800

    complete challenge3.js

--- challenge3.js
+++ challenge3.js
@@ -4,8 +4,42 @@
 
 function LongestWord(sen) { 
 
+var position = 0;
 //first find word...
+function FindWord(sen, pos)
+{
+	var word="";
+	for(i=position; i < sen.length; i++)
+	{
+		if (isalpha(sen.charAt(i)))
+		{
+			word += sen.charAt(i);
+			i++;
+			while(isalpha(sen.charAt(i)))
+			{
+				word += sen.charAt(i);
+				i++;
+			}
+			break;
+		}
+	}
+	position = i;
+	return word;
+}
 
+
+
+var largest = FindWord(sen,position);
+var second_largest ="";
+while(position < sen.length)
+{
+	second_largest=FindWord(sen,position);
+	if(second_largest.length > largest.length)
+	{	
+		largest = second_largest;
+	}
+}
+	return largest;
 
 }
    
@@ -13,6 +47,24 @@ function LongestWord(sen) {
 // to see how to enter arguments in JavaScript scroll down
 LongestWord(readline());                            
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
 
 
 
