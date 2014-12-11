17e559d2451ae55cccbe0090c59a81c84461dd08
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-10 17:59:22 -0800

    completed challenge2.js

---
+++ challenge2.js
@@ -0,0 +1,14 @@
+function FirstFactorial(num) { 
+
+if(num ==1)
+	return 1;
+  // code goes here  
+  return num*FirstFactorial(num-1); 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstFactorial(readline());                            
+
+
