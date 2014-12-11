f4431843055f5ec3d718e554dc31cc323e29927b
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-11 01:31:36 -0800

    add isalpha function to challenge3.js

---
+++ challenge3.js
@@ -0,0 +1,33 @@
+  function isalpha(c) {
+    return (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')));
+  }
+
+function LongestWord(sen) { 
+
+//first find word...
+
+
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+LongestWord(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
