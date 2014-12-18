f17f531dfdfa1f1c42eacbf4067fd6379a3d6879
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-17 20:57:26 -0800

    another solution to challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -12,3 +12,41 @@ if(num ==1)
 FirstFactorial(readline());                            
 
 
+
+/*
+function FirstFactorial(num) { 
+
+  var count = num-1;
+  var result = num;
+  while(count >= 1)
+  {
+    result = result * count;
+    count--;
+  }
+  return result;
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstFactorial(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
+
+*/
