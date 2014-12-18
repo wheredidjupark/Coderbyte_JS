0fe1bea8689ad26b9fcab10bac1f7ee37706fb8a
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-18 01:55:59 -0800

    challenge13.js

---
+++ challenge13.js
@@ -0,0 +1,29 @@
+function WordCount(str) { 
+
+  var words = str.split(" ");
+  return words.length;
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+WordCount(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
