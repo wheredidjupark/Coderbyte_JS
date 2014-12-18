31e20cba8c43df79204f8e150a4360e03cb22287
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-18 01:43:50 -0800

    complete challenge9.js

---
+++ challenge9.js
@@ -0,0 +1,31 @@
+function TimeConvert(num) { 
+  var hours = Math.floor(num/60);
+  var minutes = num%60;
+  
+  var timeInString = hours + ":" + minutes;
+  return timeInString;
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+TimeConvert(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
