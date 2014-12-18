88ce90a61d18067b88b5811ac0ea82756ba647a0
Ju Hyeon Park <wheredidjupark@gmail.com>
2014-12-17 20:57:14 -0800

    another solution to challenge1.js

--- challenge1.js
+++ challenge1.js
@@ -17,9 +17,38 @@ FirstReverse(readline());
 
 
 
+/*
+
+function FirstReverse(str) { 
+
+  var string = "";
+  for(var i=str.length-1; i >= 0; i--)
+  {
+    string += str[i];
+  }
+  return string; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+FirstReverse(readline());                            
+*/
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+                            
 
 
 
